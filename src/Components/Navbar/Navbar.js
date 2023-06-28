cbd9a2056e7eef7f005fc9b423a4ee5dc3093a03
simulador de compras online <laricu72@gmail.com>
2023-06-27 20:03:29 -0400

    subida github

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <nav>
+            <h3>ecommerce</h3>
+            <div>
+                <button>Verduras</button>
+                <button>Frutas</button>
+                <button>Otras</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
